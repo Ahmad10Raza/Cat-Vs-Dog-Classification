6b7e11bb7b1c0a1162762f30f826b6de276b4b4d
Ahmad10Raza <rjaahmad60@gmail.com>
2023-12-20 17:49:07 +0530

    Day_020

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cat-Vs-Dog-Classification",
+    version="0.0.1",
+    author="Ahmad10Raza",
+    author_email="rjaahmad60@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
